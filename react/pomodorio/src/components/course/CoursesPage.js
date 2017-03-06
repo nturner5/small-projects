0e8e4d128252f8df43b27d3ab3d1c719cc889edc
Jon Myrick <jonmyrick7401@gmail.com>
2017-03-06 11:16:12 -0700

    Added check component for more practice with svg

--- react/pomodorio/src/components/course/CoursesPage.js
+++ react/pomodorio/src/components/course/CoursesPage.js
@@ -1,10 +1,12 @@
 import React, {propTypes} from 'react';
+import Check from './Check';
 
 class CoursesPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Courses</h1>
+        <h1>Animated Check</h1>
+        <Check />
       </div>
     );
   }
